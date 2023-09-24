2e4ec7ec4d9d4e866909b9a47362243c0a2caa83
Gabriel Sirota <gsirota@frba.utn.edu.ar>
2023-09-24 12:42:46 -0300

    Actualizo cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  // projectId: "4ph3j7",
   video: true,
   reporter: 'mochawesome',
   e2e: {
+    supportFile: false,
     setupNodeEvents(on, config) {
     },
   },
